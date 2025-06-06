4853b12186947e21787fc3f5f428a05b929d44cf
kessedor <34983176+kessedor@users.noreply.github.com>
2025-06-06 12:42:44 -0700

    Update main.tsx with Sentry integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import * as Sentry from '@sentry/react';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+Sentry.init({
+  dsn: "https://6f571143f13c79fe35868f0aa9b5e58e@o4509443228303360.ingest.de.sentry.io/4509453369737296",
+  integrations: [new Sentry.BrowserTracing()],
+  tracesSampleRate: 1.0,
+  sendDefaultPii: true,
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
