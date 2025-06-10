fbadbcde0be72961f25df2f0caadf97707c1a622
kessedor <34983176+kessedor@users.noreply.github.com>
2025-06-10 09:00:41 -0700

    Updated package-lock.json

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import './index.css';
 
 Sentry.init({
   dsn: "https://6f571143f13c79fe35868f0aa9b5e58e@o4509443228303360.ingest.de.sentry.io/4509453369737296",
-  integrations: [new Sentry.BrowserTracing()],
+  integrations: [new BrowserTracing()],
   tracesSampleRate: 1.0,
   sendDefaultPii: true,
 });
